4ec21b4bef92041bebb0e573172e6243a5cbcad1
kk2453 <kartikayshukla3305@gmail.com>
2024-08-29 00:48:51 +0530

    tired

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,24 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <main className="text-red-500">
+    <div className="h-full bg-[url('/wallpaper-background-image2.jpg')] bg-bottom bg-cover">
+      <Head>
+        <title></title>
+        <meta />
+        <link rel="" href="" />
+      </Head>
+
+      
+      <header className="flex justify-between item-center h-[70px] px-5 py-4">
+        <div>hm</div>
+        <div>Wallpaper</div>
+        <div className="rounded-2xl bg-white px-3 py-2">Sign in</div>
+      </header>
+
+      <main className="">
         <p>Placeholder for main</p>
       </main>
+
       <footer>
         <p>Placeholder for footer</p>
       </footer>
